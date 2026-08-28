a89a66a02aecddfc7a173b2def6ce595b00fc0b9
★Caio Gomes★ <caiogbcorrea791@gmail.com>
2026-08-28 08:50:48 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿// Classe 1: Retângulo
+Retangulo retangulo1 = new Retangulo(-20, -12);
+Console.WriteLine(retangulo1);
---
+++ Retangulo.cs
@@ -0,0 +1,54 @@
+class Retangulo
+{
+    private double baseR;
+    private double alturaR;
+    public double BaseR
+    {
+        get { return baseR; }
+        set
+        {
+            if (value <= 0)
+            {
+                Console.WriteLine("Base inválida");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Definindo base...");
+                BaseR = value; 
+            }
+        }
+    }
+    public double AlturaR
+    {
+        get { return alturaR; }
+        set { baseR = value; }
+    }
+    public Retangulo(double baseR, double alturaR)
+    {   
+        if (baseR <= 0 || alturaR <= 0)
+        {
+            Console.WriteLine("Base e/ou altura inválida(s)");
+        }
+        else if (alturaR == baseR)
+        {
+            Console.WriteLine("Altura não pode ser igual a base");
+        }
+        else
+        {
+            this.baseR = baseR;
+            this.alturaR = alturaR;           
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"Base do Retângulo: {this.baseR} || Altura do Retângulo: {this.alturaR}";
+    }
+
+    public void CalcularArea()
+    {
+        Console.Write("A área do Retângulo é: ");
+        Console.WriteLine(baseR * alturaR);
+    }
+}
